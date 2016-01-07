eb8e8d3dd19ba118b866383d1a5377213c91ebcb
aQuigs <aQuigs@users.noreply.github.com>
2016-01-07 14:49:50 -0500

    DataTables implementation and adding records to tracker

--- js/ouid.js
+++ js/ouid.js
@@ -1,23 +1,82 @@
+function getDateFormatted() {
+	var date = new Date();
+    return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 
-    document.getElementById('tracker-button').addEventListener('click', function() {
+	var dt;
+    $('#tracker-button').on('click', function() {
 		$('#ui-staff').hide();
 		$('#ui-event').hide();
 		$('#header').text("Event Tracker");
 		$('#ui-tracker').show();
+
+		// fix to accomodate for lack of resizing table headers when display: none;
+		if (dt) {
+			dt.columns.adjust();
+		}
     });
     
-    document.getElementById('staff-button').addEventListener('click', function() {
+    $('staff-button').on('click', function() {
 		$('#ui-event').hide();
 		$('#ui-tracker').hide();
 		$('#header').text("Staff Manager");
 		$('#ui-staff').show();
     });
     
-    document.getElementById('event-button').addEventListener('click', function() {
+    $('#event-button').on('click', function() {
 		$('#ui-staff').hide();
 		$('#ui-tracker').hide();
 		$('#header').text("Event Manager");
 		$('#ui-event').show();
+    });
+
+    $('#event-name').on('change', function() {
+    		console.log($('#event-name').val());
+    	if ($('#event-name').val()) {
+    		$('#event-error').css('visibility', 'hidden');
+    	}
+    });
+
+    $('#staff-name').on('change', function() {
+    	if ($('#staff-name').val()) {
+    		$('#staff-error').css('visibility', 'hidden');
+    	}
+    });
+
+	dt = $('#data-table').DataTable({
+        "scrollY":        "400px",
+        "scrollCollapse": true,
+        // "data": data,
+        "paging":         false
+    });
+
+    $('#add-record-btn').on('click', function() {
+    	if (dt) {
+    		var gnum = $('#gnumber').val();
+    		var eventName = $('#event-name').val();
+    		var staffName = $('#staff-name').val();
+
+    		if (gnum && eventName && staffName) {
+    			dt.row.add([
+    				gnum,
+    				getDateFormatted(),
+    				eventName,
+    				staffName
+				]).draw(false);
+
+
+    			$('#gnumber').val('');
+    		} else {
+    			if (!eventName) {
+    				$('#event-error').css('visibility', 'visible');
+    			}
+
+    			if (!staffName) {
+    				$('#staff-error').css('visibility', 'visible');
+    			}
+    		}
+    	}
     });
 });
